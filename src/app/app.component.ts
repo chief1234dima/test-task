22e50026b82705d738363faa917c07c771cf74e7
Dmytro_Sahun <dmytro_sahun@epam.com>
2024-10-10 15:34:35 +0400

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { CheckUserResponseData } from './shared/interface/responses';
 
 @Component({
   selector: 'app-root',
   template: '<tt-form-container></tt-form-container>',
 })
 export class AppComponent {
-  constructor(private http: HttpClient) {}
+  constructor() {}
 }
